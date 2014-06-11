896a4be627ab370af3345bb93e2c75e16432dd2c
ADimanova <addi_91@abv.bg>
2014-06-11 16:49:57 +0300

    extended menu navigation
    vaskos commit

---
+++ scripts/loadingScreen.js
@@ -0,0 +1,6 @@
+(function(){
+    var container = document.getElementById('loading-container');   
+    setTimeout(function(){
+        window.location.href = 'MainMenu/MainMenu.html';
+    }, 2000);        
+}());
