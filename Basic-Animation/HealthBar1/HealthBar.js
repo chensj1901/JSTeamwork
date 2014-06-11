505e9dd7b0f65a6fbaf69802397834b56f4b19db
yoyofen <martinanto@abv.bg>
2014-06-11 17:15:18 +0300

    HealthBar updated position +bug
    bug appeared where the game lags and shows 2 different

--- Basic-Animation/HealthBar1/HealthBar.js
+++ Basic-Animation/HealthBar1/HealthBar.js
@@ -5,6 +5,7 @@ function HealthBar(maxhp, constantDecreasehp, fishIncrementhp) {
     this.percentageHP;
     this.constantDecreaseHP = constantDecreasehp; //tested with 1
     this.fishIncrementHP = fishIncrementhp;
+    this.paper = Raphael(canvas.offsetLeft, canvas.offsetTop, 900, 600);
 
     this.constantHPRectangleWidth = 334;
 
@@ -19,7 +20,7 @@ function HealthBar(maxhp, constantDecreasehp, fishIncrementhp) {
         //Logic when we eat a fish
         if (this.triggerFish()) {
             this.currentHP += this.fishIncrementHP;
-            console.log("The Shark ate a fish")
+          //  console.log("The Shark ate a fish")
         }
         //if we have more hp than we should;
         if (this.currentHP > this.maxHP) {
@@ -39,20 +40,19 @@ function HealthBar(maxhp, constantDecreasehp, fishIncrementhp) {
     }
 
     this.draw=function() {
-        var paper = Raphael(0, 0, 900, 600);
-        var outerRect = paper.rect(30, 540, 340, 23, 3);
+        var outerRect = this.paper.rect(6, 6, 340, 23, 3);
         outerRect.attr({
             fill: "0-#dadada:0-#f3f3f3:100",
             stroke: "#7e7e7e",
             "stroke-width": 0.5
         });
-        var innerRect = paper.rect(33, 543, 334, 17, 2);
+        var innerRect = this.paper.rect(9, 9, 334, 17, 2);
         innerRect.attr({
             fill: "0-#dadada:0-#f3f3f3:100",
             stroke: "#7e7e7e",
             "stroke-width": 0.5
         });
-        var healthPointsRect = paper.rect(33, 543, this.constantHPRectangleWidth * this.percentageHP, 17, 2);
+        var healthPointsRect = this.paper.rect(9, 9, this.constantHPRectangleWidth * this.percentageHP, 17, 2);
         healthPointsRect.attr({
             fill: "0-#c14232:0-#f6928b:100",
             stroke: "#7e7e7e",
