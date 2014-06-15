9e9719440cce1badda90269bee4335a5f216379f
ADimanova <addi_91@abv.bg>
2014-06-15 13:01:55 +0300

    NewGame fixed and added animation rate

--- Hungry-Shark/main.js
+++ Hungry-Shark/main.js
@@ -10,6 +10,9 @@ var biteSound = new Audio('sounds/bite.wav');
 var oceanFloorBackground = new BackgroundFeature(context, 'images/ocean-floor.png', 370, 4);
 var boatBackground = new BackgroundFeature(context, 'images/boat.png', 42, 2);
 
+var resetShark = true;
+var fps = 5;
+
 // User interaction
 function onMouseClickEvent (event) {
     if (gameState === GameStatesEnum.HIGH_SCORES || gameState === GameStatesEnum.CREDITS) {
@@ -32,6 +35,7 @@ function onMouseClickEvent (event) {
     if (newGameChoice) {
         biteSound.play();		
         gameState = GameStatesEnum.GAME_ON;
+		resetShark = true;
         enterGameState(gameState);
     }
 
@@ -78,28 +82,36 @@ function drawCanvasTopBorder (positionY) {
 }
 
 function drawFrame () {
-    if (gameState === GameStatesEnum.GAME_ON) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        oceanFloorBackground.draw();
-        shark.update();
-        shark.draw();
-        prey.update();
-        prey.draw();
-        healthBar.update();
-        healthBar.draw();
-        scores.update();
-        scores.draw();
-        boatBackground.draw();
-        drawCanvasTopBorder(40);
+	setInterval(function() {
+		if (gameState === GameStatesEnum.GAME_ON) {
+				if(resetShark) {
+					resetShark = false;
+					shark = new Shark(context, 150, 250);
+					healthBar = new HealthBar(3000, 3, 500);
+					scores = new Scores();
+				}
+				context.clearRect(0, 0, canvas.width, canvas.height);
+				oceanFloorBackground.draw();
+				shark.update();
+				shark.draw();
+				prey.update();
+				prey.draw();
+				healthBar.update();
+				healthBar.draw();
+				scores.update();
+				scores.draw();
+				boatBackground.draw();
+				drawCanvasTopBorder(40);
 
-        window.requestAnimationFrame(drawFrame, canvas);
-    }
+				window.requestAnimationFrame(drawFrame, canvas);
+		}
 
-    if( shark.y < 60 || shark.y > 570 ) {
-        console.log('over');
-        gameState = GameStatesEnum.GAME_OVER;
-        enterGameState(gameState);
-    }
+		if( shark.y < 40 || shark.y > 570 ) {
+			console.log('over');
+			gameState = GameStatesEnum.GAME_OVER;
+			enterGameState(gameState);
+		}
+	}, 1000 / fps);
 }
 
 // Game states
