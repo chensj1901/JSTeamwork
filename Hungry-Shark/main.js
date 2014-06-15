6ddb2276e2bd734a79593d64aa21dc0b7647df14
ADimanova <addi_91@abv.bg>
2014-06-15 13:46:04 +0300

    bug fix trial

--- Hungry-Shark/main.js
+++ Hungry-Shark/main.js
@@ -11,7 +11,6 @@ var oceanFloorBackground = new BackgroundFeature(context, 'images/ocean-floor.pn
 var boatBackground = new BackgroundFeature(context, 'images/boat.png', 42, 2);
 
 var resetShark = true;
-var fps = 5;
 
 // User interaction
 function onMouseClickEvent (event) {
@@ -82,36 +81,34 @@ function drawCanvasTopBorder (positionY) {
 }
 
 function drawFrame () {
-	setInterval(function() {
-		if (gameState === GameStatesEnum.GAME_ON) {
-				if(resetShark) {
-					resetShark = false;
-					shark = new Shark(context, 150, 250);
-					healthBar = new HealthBar(3000, 3, 500);
-					scores = new Scores();
-				}
-				context.clearRect(0, 0, canvas.width, canvas.height);
-				oceanFloorBackground.draw();
-				shark.update();
-				shark.draw();
-				prey.update();
-				prey.draw();
-				healthBar.update();
-				healthBar.draw();
-				scores.update();
-				scores.draw();
-				boatBackground.draw();
-				drawCanvasTopBorder(40);
+	if (gameState === GameStatesEnum.GAME_ON) {
+			if(resetShark) {
+				resetShark = false;
+				shark = new Shark(context, 150, 250);
+				healthBar = new HealthBar(3000, 3, 500);
+				scores = new Scores();
+			}
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			oceanFloorBackground.draw();
+			shark.update();
+			shark.draw();
+			prey.update();
+			prey.draw();
+			healthBar.update();
+			healthBar.draw();
+			scores.update();
+			scores.draw();
+			boatBackground.draw();
+			drawCanvasTopBorder(40);
 
-				window.requestAnimationFrame(drawFrame, canvas);
-		}
+			window.requestAnimationFrame(drawFrame, canvas);
+	}
 
-		if( shark.y < 40 || shark.y > 570 ) {
-			console.log('over');
-			gameState = GameStatesEnum.GAME_OVER;
-			enterGameState(gameState);
-		}
-	}, 1000 / fps);
+	if( shark.y < 40 || shark.y > 570 ) {
+		console.log('over');
+		gameState = GameStatesEnum.GAME_OVER;
+		enterGameState(gameState);
+	}
 }
 
 // Game states
